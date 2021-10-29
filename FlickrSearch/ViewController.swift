4618ea22ee04ec1a770db7cfab4db2b5fe8627f5
Alex Vasyliev <olorin2007@gmail.com>
2021-10-29 15:07:08 +0300

    initial commit

---
+++ FlickrSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlickrSearch
+//
+//  Created by Oleksii Vasyliev on 29.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
